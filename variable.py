ead18cde01cf900beefeab508dbee43eee4fcc88
Googleok <qkrwhddjr3@gmail.com>
2019-06-12 13:48:22 +0900

    변수, 확장, 치환문

---
+++ variable.py
@@ -0,0 +1,22 @@
+# 변수 이름은 문자, 숫자, _ 로 구성된다.
+import keyword
+
+friend = 1
+a = 10
+my_name = '박종억'
+myName = '박종억'
+_yourname = '박소원'
+member1 = '박소원'
+
+# 에러
+# frineds$ = 1
+# a! = 10
+# 1abc = 10
+# def = 10
+
+print(keyword.kwlist)
+
+# 한글이름의 변수도 사용이 가능하다.
+가격1 = 10000
+print(가격1 - 5000)
+
